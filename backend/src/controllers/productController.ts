83015db09a3e77249e78b855b4f6536d31643280
Kandy-Hamisi <hkandy2000@gmail.com>
2026-01-05 10:47:50 +0300

    finalize routes and controllers setup

---
+++ backend/src/controllers/productController.ts
@@ -0,0 +1,133 @@
+import type { Request, Response } from "express";
+import * as queries from "../db/queries";
+
+import { getAuth } from "@clerk/express";
+
+// get all products (public)
+export const getAllProducts = async (req: Request, res: Response) => {
+  try {
+    const products = await queries.getAllProducts();
+    res.status(200).json(products);
+  } catch (err) {
+    console.error("Error fetching products: ", err);
+    res.status(500).json({ error: "Failed to fetch products" });
+  }
+};
+
+// get single product by ID (public)
+export const getProductById = async (req: Request, res: Response) => {
+  try {
+    const { id } = req.params;
+    const product = await queries.getProductById(id);
+
+    if (!product) return res.status(404).json({ error: "Product not found" });
+
+    res.status(200).json(product);
+  } catch (error) {
+    console.error("Error fetching product: ", error);
+    res.status(500).json({ error: "Failed to fetch product" });
+  }
+};
+
+// get products by current user (protected)
+
+export const getMyProducts = async (req: Request, res: Response) => {
+  try {
+    const { userId } = getAuth(req);
+    if (!userId) return res.status(401).json({ error: "Unauthorized" });
+
+    const products = await queries.getProductsByUserId(userId);
+    res.status(200).json(products);
+  } catch (error) {
+    console.error("Error fetching user products: ", error);
+    res.status(500).json({ error: "Failed to fetch user products" });
+  }
+};
+
+// Create product (protected)
+export const createProduct = async (req: Request, res: Response) => {
+  try {
+    const { userId } = getAuth(req);
+    if (!userId) return res.status(401).json({ error: "Unauthorized" });
+
+    const { title, description, price, imageUrl } = req.body;
+
+    if (!title || !description || !price || !imageUrl) {
+      res.status(400).json({ error: "Missing required fields" });
+      return;
+    }
+
+    const product = await queries.createProduct({
+      userId,
+      title,
+      description,
+      price,
+      imageUrl,
+    });
+
+    res.status(201).json(product);
+  } catch (error) {
+    console.error("Error creating product: ", error);
+    res.status(500).json({ error: "Failed to create product" });
+  }
+};
+
+// Update product (protected - owner only)
+export const updateProduct = async (req: Request, res: Response) => {
+  try {
+    const { userId } = getAuth(req);
+    if (!userId) return res.status(401).json({ error: "Unauthorized" });
+
+    const { id } = req.params;
+    const { title, description, price, imageUrl } = req.body;
+
+    // check if product exists and belongs to user
+    const existingProduct = await queries.getProductById(id);
+    if (!existingProduct) {
+      res.status(404).json({ error: "Product not found" });
+      return;
+    }
+
+    if (existingProduct.userId !== userId) {
+      res.status(403).json({ error: "You can only update your own products" });
+    }
+
+    const updatedProduct = await queries.updateProduct(id, {
+      title,
+      description,
+      price,
+      imageUrl,
+    });
+    res.status(200).json(updatedProduct);
+  } catch (error) {
+    console.error("Error updating product: ", error);
+    res.status(500).json({ error: "Failed to update product" });
+  }
+};
+
+// Delete product (protected - owner only)
+export const deleteProduct = async (req: Request, res: Response) => {
+  try {
+    const { userId } = getAuth(req);
+    if (!userId) return res.status(401).json({ error: "Unauthorized" });
+
+    const { id } = req.params;
+
+    //  check if product exists and belongs to user
+    const existingProduct = await queries.getProductById(id);
+    if (!existingProduct) {
+      res.status(404).json({ error: "Product not found" });
+      return;
+    }
+
+    if (existingProduct.userId !== userId) {
+      res.status(403).json({ error: "You can only delete your own products" });
+    }
+
+    const deletedProduct = await queries.deleteProduct(id);
+    res.status(200).json(deletedProduct);
+  } catch (error) {
+    console.error("Error deleting product: ", error);
+    res.status(500).json({ error: "Failed to delete product" });
+  }
+};
--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,23 +1,30 @@
 import express from "express";
 import cors from "cors";
-import {ENV} from "./config/env";
+import { ENV } from "./config/env";
 import { clerkMiddleware } from "@clerk/express";
 
 // import application routes
+import userRoutes from "./routes/userRoutes";
+import productRoutes from "./routes/productRoutes";
+import commentRoutes from "./routes/commentRoutes";
 
 const app = express();
 
 // third party middlewares
-app.use(cors({
-    origin: ENV.FRONTEND_URL
-}))
-app.use(clerkMiddleware());  //auth object will be attached to the req
+app.use(
+  cors({
+    origin: ENV.FRONTEND_URL,
+  }),
+);
+app.use(clerkMiddleware()); //auth object will be attached to the req
 app.use(express.json()); //parses json request bodies
-app.use(express.urlencoded({extended: true})); // parses form data (like HTML forms)
+app.use(express.urlencoded({ extended: true })); // parses form data (like HTML forms)
 
 // application routes
-
+app.use("/api/users", userRoutes);
+app.use("/api/products", productRoutes);
+app.use("/api/comments", commentRoutes);
 
 const server = app.listen(ENV.PORT, () => {
-    console.log("Server is running on port 3000");
+  console.log("Server is running on port 3000");
 });
